718551124d9889edc043b286c1557590706c83d4
hieunt84 <hieunt9@gmail.com>
2021-04-14 10:12:32 +0700

    Update and rename restart-cluster-galera.sh to start-cluster-galera.sh

--- restart-cluster-galera.sh
+++
@@ -1,7 +0,0 @@
-# when all node shuttdowned
-
-# power all node in cluster thì mới start được cluser như bên dưới
-
-# on node in cluster , thường là node 1
-galera_recovery
-systemctl start mariadb
